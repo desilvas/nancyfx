1ecbd1a2d29c06d97e30266bed95ab18d08f39fd
shalinds <shalinds@gmail.com>
2019-06-02 21:12:06 +0530

    first commit

---
+++ Nancy.Api/HelloWorldModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nancy.Api
+{
+    public class HelloWorldModule : NancyModule
+    {
+        public HelloWorldModule()
+        {
+            Get("v1/hello/{name}", args =>
+            {
+                string name = args.name;
+                var response = string.Format("Hello {0} ! ", name);
+                return response;
+            });
+        }
+    }
+}
---
+++ Nancy.Api/TestHelloWorld.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nancy.Api
+{
+    [TestClass]
+    public class TestHelloWorld
+    {
+
+        private Browser _browser;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var bootstrapper = new DefaultNancyBootstrapper();
+            _browser = new Browser(bootstrapper);
+        }
+
+        [TestCleanup]
+        public void MyTestCleanup()
+        {
+            _browser = null;
+        }
+
+        [TestMethod]
+        public void If_HelloWord_Is_Called_Then_Return_OK_And_ExpectedResult()
+        {
+            var name = "Test";
+            var expectedResponse = string.Format("Hello {0} ! ", name);
+
+            var route = "/v1/hello/{0}";
+            var request = string.Format(route, name);
+            var result = _browser.Get(request, with =>
+                 {
+                     with.HttpRequest();
+                 });
+
+            var response = result.Result;
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.IsTrue(response.Body.AsString().Equals(expectedResponse));
+        }
+    }
+}
